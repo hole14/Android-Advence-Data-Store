7e0cf5b7a4eb426393dfc742277e97134fecce3f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-04 10:06:01 +0700

    Menambahkan

--- app/src/main/java/com/example/mydatastore/MainActivity.kt
+++ app/src/main/java/com/example/mydatastore/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.example.mydatastore
 
 import android.os.Bundle
+import android.widget.CompoundButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        val pref = SettingPreferences.getInstance(application.dataStore)
+        val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            MainViewModel::class.java
+        )
+
+        mainViewModel.getThemeSetting().observe(this){isDarkModeActive: Boolean ->
+            if (isDarkModeActive){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switch.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switch.isChecked = false
+            }
+        }
+
+        switch.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
+        }
 
     }
 }
